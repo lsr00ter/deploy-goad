9dc0b06d96a1df4012f59b6cab9ec3b174e4b379
lsr00ter <codejarry@gmail.com>
2024-11-28 13:24:12 +0800

    update virtualbox to 7+

--- deploy-goad.sh
+++ deploy-goad.sh
@@ -2,7 +2,18 @@
 echo "Deploy GOAD v2 on Ubuntu 22.04"
 
 # Ensure we're on the right OS and version
-if [ "`lsb_release -sd | cut -c -12`" != "Ubuntu 22.04" ]; then
+source /etc/os-release
+if [ "$ID" = "ubuntu" ]; then
+  IFS='.' read -r -a version_parts <<<"$VERSION_ID"
+  major_version=${version_parts[0]}
+  minor_version=${version_parts[1]}
+  if [ "$major_version" -gt 22 ] || { [ "$major_version" -eq 22 ] && [ "$minor_version" -ge 4 ]; }; then
+    echo "Ubuntu version is 22.04 or above."
+  else
+    echo "This script must be run on Ubuntu 22.04"
+    exit 1
+  fi
+else
   echo "This script must be run on Ubuntu 22.04"
   exit 1
 fi
@@ -23,20 +34,30 @@ apt-get update
 apt-get -y dist-upgrade
 
 # Check if we're running inside VirtualBox
-if [ `dmidecode -s system-product-name` = "VirtualBox" ]; then
+if [ $(dmidecode -s system-product-name) = "VirtualBox" ]; then
   # Install VirtualBox guest additions
   apt-get install -y virtualbox-guest-utils virtualbox-guest-x11
 fi
 
+# Install Virtualbox 7+
+wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | gpg --dearmor --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian jammy contrib" | tee /etc/apt/sources.list.d/virtualbox.list
+apt update
+apt install virtualbox-7.0 -y
+apt install gcc-12 -y
+wget https://download.virtualbox.org/virtualbox/7.0.14/Oracle_VM_VirtualBox_Extension_Pack-7.0.14.vbox-extpack
+VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-7.0.14.vbox-extpack
+
 # Install base packages needed
-apt-get install -y git virtualbox python3-pip
+apt-get install -y git python3-pip pipx
+pipx ensurepath && source /root/.bashrc
 
 # Enable IP forwarding on Ubuntu
 yes | apt-get install iptables-persistent
 DEFAULT_ETH=$(ip route get 8.8.8.8 | sed -n 's/.*dev \([^\ ]*\).*/\1/p')
-if [ "`cat /proc/sys/net/ipv4/ip_forward`" != "1" ]; then
+if [ "$(cat /proc/sys/net/ipv4/ip_forward)" != "1" ]; then
   # Implement in sysctl
-  echo net.ipv4.ip_forward = 1 >> /etc/sysctl.conf
+  echo net.ipv4.ip_forward = 1 >>/etc/sysctl.conf
   sysctl -p
   iptables -A FORWARD -i $DEFAULT_ETH -o vboxnet0 -j ACCEPT
   iptables -A FORWARD -i vboxnet0 -o $DEFAULT_ETH -m state --state ESTABLISHED,RELATED -j ACCEPT
@@ -45,15 +66,16 @@ if [ "`cat /proc/sys/net/ipv4/ip_forward`" != "1" ]; then
 fi
 
 # Check if vagrant is installed
-if ! dpkg -s vagrant &> /dev/null; then
-  wget https://releases.hashicorp.com/vagrant/2.4.0/vagrant_2.4.0-1_amd64.deb
-  dpkg --install vagrant_2.4.0-1_amd64.deb
+if ! dpkg -s vagrant &>/dev/null; then
+  wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+  echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
+  apt update && yes | apt install vagrant
 fi
 
 # Set up prerequisites, not doing a venv but could be changed to that
-pip install --upgrade pip
-pip install ansible-core==2.12.6
-pip install pywinrm
+# pip install --upgrade pip
+pipx install ansible-core
+pipx install pywinrm --include-deps
 
 # Install stuff needed for Vagrant
 vagrant plugin install winrm
@@ -61,7 +83,7 @@ vagrant plugin install winrm-elevated
 
 # Download GOAD
 if [ ! -d /opt/goad ]; then
-  git clone https://github.com/lkarlslund/GOAD /opt/goad
+  git clone --depth=1 https://github.com/Orange-Cyberdefense/GOAD /opt/goad
 fi
 
 # Install GOAD stuff needed for Ansible
@@ -76,4 +98,9 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
+# Load windows-update-disabler for next step
+git clone --depth=1 https://github.com/tsgrgo/windows-update-disabler /opt/windows-update-disabler
+
 echo "Deployment succeeded, your lab is now up and running on the 192.168.56.0/24 network"
+echo "Upload '/opt/windows-update-disabler/' folder to each host, run 'disable updates.bat' to prevent windows update"
+echo "Start VMs with: 'sudo su && cd /opt/goad/ad/GOAD/providers/virtualbox && vagrant up'"
